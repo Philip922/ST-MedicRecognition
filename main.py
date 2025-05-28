415645d792ffef5daea34bd70095853a8b194a25
Philip <Philipwork@zohomail.com>
2025-05-27 21:39:14 -0500

    Added cors to API

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ from pydantic import BaseModel
 from mutagen import MutagenError
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or ["http://localhost:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 load_dotenv(dotenv_path=dotenv_path)
